69ae7fed8a8e55440a36ab545fecb39c96f41fe2
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2023-06-09 19:17:57 +0100

    while loop prompt for NaNs
    fixed a typo in the file address, console.logged to check if the code was generating a random number with no decimals - all good. added a while loop in case a NaN is typed, so a prompt is looped until an actual number is typed in.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let maximum = parseInt(prompt("Enter the maximum number for our game."));
+while (!maximum) {
+  maximum = parseInt(prompt("Please, enter a valid number."));
+}
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
