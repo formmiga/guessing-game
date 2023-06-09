88978f7fce0e52df6dd4843dbad847ae438f2231
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2023-06-09 19:34:12 +0100

    console.log message
    updated the console.log message to actually display the number of attempts. to do that, created a new variable with the name attempts, set it to start at 1, then added it to the while loop so every time the loop begins, one new attempt is added. wrote the message in a string template literal to make it easier to add in the attempts variable.

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
 let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
 
 while (guess !== targetNum) {
+  attempts++;
   if (guess > targetNum) {
     guess = parseInt(prompt("Too high! Try again."));
   } else {
     guess = parseInt(prompt("Too low! Try again."));
   }
 }
-console.log("You got it, dude ;)");
+console.log(
+  `You got it, dude ;) It just took you ${attempts} guess(es) to win. Whoop!`
+);
